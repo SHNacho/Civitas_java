fb156ba4e4478bf0cde275508f043d3f8f194723
SHNacho <nachosanchezh@hotmail.com>
2019-09-19 14:31:30 +0200

    first commit

---
+++ src/civitas/Tablero.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package civitas;
+
+/**
+ *
+ * @author nacho
+ */
+public class Tablero {
+    
+    private int numCasillaCarcel;
+    private ArrayList<Casilla> casillas;
+    private int porSalida;
+    private boolean tieneJuez;
+}
